0e17c044064d931db1afe04afa637699e3010fe0
Caleb <caleb.santiago.04@gmail.com>
2026-06-23 16:00:30 -0500

    Se corrigieron registros de modelo de admin y mapeo de app

--- apps/usuarios/admin.py
+++ apps/usuarios/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import Usuario
 
-# Register your models here.
+# añadimos una configuración propia para el admin para no usar la de django por defecto
+try:
+    admin.site.unregister(Usuario)
+except admin.sites.NotRegistered:
+    pass
+
+@admin.register(Usuario)
+class UsuarioAdmin(UserAdmin):
+    list_display = ['username', 'email', 'first_name', 'last_name', 'rol', 'is_staff']
+    list_filter = ['rol', 'is_staff', 'is_active']
+    
+    fieldsets = UserAdmin.fieldsets + (
+        ('Información de Roles de la EAN', {
+            'fields': ('rol', 'telefono', 'activo'),
+        }),
+    )
