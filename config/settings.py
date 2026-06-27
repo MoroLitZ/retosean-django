71a89831f1ef8c8580e15758dbb08adf2a7b2524
Caicedogamer <juancame08@gmail.com>
2026-06-26 23:32:25 -0500

    feat(retos): implement sprint 3 and 4 on caleb

--- config/settings.py
+++ config/settings.py
@@ -15,6 +15,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'apps.usuarios',
+    'apps.retos',
 ]
 
 MIDDLEWARE = [
--- config/urls.py
+++ config/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('usuarios/', include('apps.usuarios.urls')),
+    path('retos/', include('apps.retos.urls')),
 ]
 
 if settings.DEBUG:
