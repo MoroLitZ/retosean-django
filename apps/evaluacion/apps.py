043f157dae6a0d0b8e3ec7f98106f1c8c2b9df57
Caleb Salazar <caleb.santiago.04@gmail.com>
2026-07-16 19:11:35 -0500

    Refactor: unificación de ramas, resolución de conflictos y corrección de sintaxis

--- apps/evaluacion/apps.py
+++ apps/evaluacion/apps.py
@@ -1,14 +1,6 @@
 from django.apps import AppConfig
 
-<<<<<<< HEAD
-
-class EvaluacionConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "apps.evaluacion"
-    verbose_name = "Evaluacion"
-=======
 class EvaluacionConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.evaluacion'
     verbose_name = 'Evaluacion'
->>>>>>> origin/Caicedo
